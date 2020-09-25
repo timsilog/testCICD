ebd4beb3d6b0b7ee691a8cb1f28e1a4b5856b66b
joset5 <jose.timothy@gene.com>
2020-09-24 18:38:44 -0700

    more text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,6 +85,7 @@
                 </div>
                 <h1>here is some text</h1>
                 <h2>here is some more text</h2>
+                <h3>omg even more text</h3>
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
