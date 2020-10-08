2460c88671a5092f0dacffb86ca9c7340f8e8114
joset5 <jose.timothy@gene.com>
2020-10-07 17:01:44 -0700

    remove

--- lib/vpc-stack.d.ts
+++
@@ -1,7 +0,0 @@
-import { App, Stack, StackProps } from '@aws-cdk/core';
-import { SecurityGroup, Vpc } from '@aws-cdk/aws-ec2';
-export declare class VpcStack extends Stack {
-    readonly vpc: Vpc;
-    readonly egressSecurityGroup: SecurityGroup;
-    constructor(scope: App, id: string, props?: StackProps);
-}
