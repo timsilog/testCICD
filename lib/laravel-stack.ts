d35f28f4e5349c153e191f6ed7fdb9a446887346
joset5 <jose.timothy@gene.com>
2020-10-08 17:46:21 -0700

    please work

--- lib/laravel-stack.ts
+++ lib/laravel-stack.ts
@@ -12,6 +12,56 @@ import * as efs from '@aws-cdk/aws-efs';
 import { LambdaApplication } from "@aws-cdk/aws-codedeploy";
 import config from '../config';
 import { Aws } from "@aws-cdk/core";
+import { load } from "ts-dotenv";
+
+const env = load({
+    APP_NAME: String,
+    APP_ENV: String,
+    APP_KEY: String,
+    APP_DEBUG: String,
+    APP_URL: String,
+
+    LOG_CHANNEL: String,
+
+    DB_CONNECTION: String,
+    DB_HOST: String,
+    DB_PORT: String,
+    DB_DATABASE: String,
+    DB_USERNAME: String,
+    // DB_PASSWORD: String,
+
+    BROADCAST_DRIVER: String,
+    CACHE_DRIVER: String,
+    QUEUE_CONNECTION: String,
+    SESSION_DRIVER: String,
+    SESSION_LIFETIME: String,
+
+    REDIS_HOST: String,
+    REDIS_PASSWORD: String,
+    REDIS_PORT: String,
+
+    MAIL_MAILER: String,
+    MAIL_HOST: String,
+    MAIL_PORT: String,
+    MAIL_USERNAME: String,
+    MAIL_PASSWORD: String,
+    MAIL_ENCRYPTION: String,
+    MAIL_FROM_ADDRESS: String,
+    MAIL_FROM_NAME: String,
+
+    // AWS_ACCESS_KEY_ID: String,
+    // AWS_SECRET_ACCESS_KEY: String,
+    AWS_DEFAULT_REGION: String,
+    AWS_BUCKET: String,
+    AWS_URL: String,
+
+    // PUSHER_APP_ID: String,
+    // PUSHER_APP_KEY: String,
+    // PUSHER_APP_SECRET: String,
+    // PUSHER_APP_CLUSTER: String,
+    // MIX_PUSHER_APP_KEY: String,
+    // MIX_PUSHER_APP_CLUSTER: String,
+});
 
 interface ApplicationStackProps extends cdk.StackProps {
     vpc: Vpc;
@@ -80,12 +130,55 @@ export class LaravelStack extends cdk.Stack {
                     resourceName: 'php-74-fpm:11',
                 }, this)),
             ],
-            environment: { // will probably use these for laravel too
-                // APP_STORAGE: '/tmp',
-                // LOG_CHANNEL: 'stderr',
-                // SESSION_DRIVER: 'array',
-                // VIEW_COMPILED_PATH: '/tmp/storage/framework/views',
-                MY_VARIABLE: 'test',
+            environment: {
+                APP_NAME: env.APP_NAME,
+                APP_ENV: env.APP_ENV,
+                APP_KEY: env.APP_KEY,
+                APP_DEBUG: env.APP_DEBUG,
+                APP_URL: env.APP_URL,
+
+                LOG_CHANNEL: env.LOG_CHANNEL,
+
+                DB_CONNECTION: env.DB_CONNECTION,
+                DB_HOST: env.DB_HOST,
+                DB_PORT: env.DB_PORT,
+                DB_DATABASE: env.DB_DATABASE,
+                DB_USERNAME: env.DB_USERNAME,
+                // DB_PASSWORD: env.DB_PASSWORD ? env.DB_PASSWORD : '',
+
+                BROADCAST_DRIVER: env.BROADCAST_DRIVER,
+                CACHE_DRIVER: env.CACHE_DRIVER,
+                QUEUE_CONNECTION: env.QUEUE_CONNECTION,
+                SESSION_DRIVER: env.SESSION_DRIVER,
+                SESSION_LIFETIME: env.SESSION_LIFETIME,
+
+                REDIS_HOST: env.REDIS_HOST,
+                REDIS_PASSWORD: env.REDIS_PASSWORD,
+                REDIS_PORT: env.REDIS_PORT,
+
+                MAIL_MAILER: env.MAIL_MAILER,
+                MAIL_HOST: env.MAIL_HOST,
+                MAIL_PORT: env.MAIL_PORT,
+                MAIL_USERNAME: env.MAIL_USERNAME,
+                MAIL_PASSWORD: env.MAIL_PASSWORD,
+                MAIL_ENCRYPTION: env.MAIL_ENCRYPTION,
+                MAIL_FROM_ADDRESS: env.MAIL_FROM_ADDRESS,
+                MAIL_FROM_NAME: env.MAIL_FROM_NAME,
+
+                // AWS_ACCESS_KEY_ID: env.AWS_ACCESS_KEY_ID ? env.AWS_ACCESS_KEY_ID : '',
+                // AWS_SECRET_ACCESS_KEY: env.AWS_SECRET_ACCESS_KEY ? env.AWS_SECRET_ACCESS_KEY : '',
+                AWS_DEFAULT_REGION: env.AWS_DEFAULT_REGION,
+                AWS_BUCKET: env.AWS_BUCKET,
+                AWS_URL: env.AWS_URL,
+
+                // PUSHER_APP_ID: env.PUSHER_APP_ID ? env.PUSHER_APP_ID : '',
+                // PUSHER_APP_KEY: env.PUSHER_APP_KEY ? env.PUSHER_APP_KEY : '',
+                // PUSHER_APP_SECRET: env.PUSHER_APP_SECRET ? env.PUSHER_APP_SECRET : '',
+                // PUSHER_APP_CLUSTER: env.PUSHER_APP_CLUSTER,
+
+                // MIX_PUSHER_APP_KEY: env.MIX_PUSHER_APP_KEY,
+                // MIX_PUSHER_APP_CLUSTER: env.MIX_PUSHER_APP_CLUSTER,
+
                 WORDPRESS_DB_ENDPOINT: props.rdsEndpoint,
                 WORDPRESS_DB_PORT: props.rdsPort,
                 WORDPRESS_DB_NAME: props.rdsDb,
