268f663b5179e9a54d1d103cbcbfec141f94f0fc
joset5 <jose.timothy@gene.com>
2020-10-13 15:26:07 -0700

    testing simple api call

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('test', function () {
+    // If the Content-Type and Accept headers are set to 'application/json', 
+    // this will return a JSON structure. This will be cleaned up later.
+    return "Hello World from Tim";
 });
